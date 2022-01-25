834ac4c2f31477916edfff25ec95d1526e8b1715
Avilio Muñoz <aviliomuoz@gmail.com>
2022-01-24 19:05:22 -0500

    Notes CRUD with pysondb

--- notes.py
+++ notes.py
@@ -1,6 +1,6 @@
-import json
-import random
 from typing import List, Dict
+
+from pysondb import db
 
 from pydantic import BaseModel
 from pydantic import Field
@@ -9,56 +9,52 @@ from fastapi import FastAPI
 from fastapi import Path, Body
 
 app = FastAPI()
-
-# Utils
-def getNotes():
-    with open("data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        return data
-
-def setNotes(data):
-    with open("data.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps(data))
-
-def getRandomID():
-    id: str = str(random.randint(1,9)) + str(random.randint(1,9)) + str(random.randint(1,9))
-    return id
+database = db.getDb("database.json")
 
 # Models
 class Note(BaseModel):
-    id: str = Field(...)
     text: str = Field(
         ..., 
         min_length=1, 
-        max_length=20, 
-        title="text note", 
-        description="a simple text note", 
+        max_length=100, 
+        title="Nota", 
+        description="Una nota de texto simple", 
         example="Nota de prueba"
     )
 
 # CRUD
 @app.get(path="/notes")
 def get_notes():
-    notes: List[Dict[str, str]] = getNotes()
-    return notes
+    notes: List[Dict[str, str]] = database.getAll()
+    return {
+        "ok": True,
+        "msg": "Todas las notas",
+        "notes": notes
+    }
 
 @app.post(path="/notes")
 def create_note(note: Note = Body(...)):
-    notes: List[Dict[str, str]] = getNotes()
-    notes.append({"id": getRandomID(), "text": note.text})
-    setNotes(notes)
-    return notes
+    database.add({"text": note.text})
+    return {
+        "ok": True,
+        "msg": "Nota creada con éxito",
+        "note": note
+    }
 
 @app.put(path="/notes/{note_id}")
-def update_note(note_id: str = Path(...), note: Note = Body(...)):
-    notes: List[Dict[str, str]] = getNotes()
-    notes = list(map(lambda n: {"id": n["id"], "text": note.text} if n["id"] == note_id else {"id": n["id"], "text": n["text"]}, notes))
-    setNotes(notes)
-    return notes
+def update_note(note_id: int = Path(...), note: Note = Body(...)):
+    database.updateById(note_id, {"text": note.text})
+    return {
+        "ok": True,
+        "msg": "Nota modificada con éxito",
+        "note": note
+    }
 
 @app.delete(path="/notes/{note_id}")
-def delete_note(note_id: str = Path(...)):
-    notes: List[Dict[str, str]] = getNotes()
-    notes = list(filter(lambda n: n["id"] != note_id, notes))
-    setNotes(notes)
-    return notes
+def delete_note(note_id: int = Path(...)):
+    database.deleteById(note_id)
+    return {
+        "ok": True,
+        "msg": "Nota eliminada con éxito",
+        "note": note_id
+    }
